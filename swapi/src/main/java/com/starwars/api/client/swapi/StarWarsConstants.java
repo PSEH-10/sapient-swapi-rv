01bfaa1850ff95d73120cea73c00a9ef2fc9d3db
PSEH-9 <samaharadurgi@gmail.com>
2019-08-10 15:12:15 +0530

    Swapi API for sapient

---
+++ swapi/src/main/java/com/starwars/api/client/swapi/StarWarsConstants.java
@@ -0,0 +1,6 @@
+package com.starwars.api.client.swapi;
+
+public class StarWarsConstants {
+    public static final String BASE_URL = "https://swapi.co/api/";
+    public static final String USER_AGENT_NAME = "StarWars-Spaient/1.0";
+}
